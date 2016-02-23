e73c6d238c5e6d3ce80d5e9a3f24908ea6bffa84
justin-ch-goh <justin.ch.goh@gmail.com>
2016-02-23 19:12:59 +0800

    all wired up

--- models/Comments.js
+++ models/Comments.js
@@ -7,4 +7,10 @@ var CommentSchema = new mongoose.Schema({
     post: {type: mongoose.Schema.Types.ObjectId, ref: 'Post'}
 });
 
+//cb refers to callback
+CommentSchema.methods.upvote = function(cb) {
+    this.upvotes += 1;
+    this.save(cb);
+};
+
 mongoose.model('Comment', CommentSchema)
--- routes/index.js
+++ routes/index.js
@@ -6,22 +6,34 @@ var Post = mongoose.model('Post');
 var Comment = mongoose.model('Comment');
 
 /* GET home page. */
-router.get('/posts', function(req, res, next) {
-  Post.find(function(err, posts){
-    if (err){return next(err);};
-    res.json(posts);
-  });
+
+// this '/' route shouldn't be deleted for some reason!
+router.get('/', function(req, res) {
+    res.render('index', {title: 'Express'});
 });
 
-router.get('posts/:post', function(req, res) {
-    res.json(req.post);
+
+router.get('/posts', function(req, res, next) {
+  Post.find(function(err, posts){
+    if (err){return next(err);}
+    res.json(posts);
+  });
 });
 
 router.post('/posts', function(req, res, next) {
     var post = new Post(req.body);
 
     post.save(function(err, post){
-        if (err){return next(err);};
+        if (err){return next(err);}
+        res.json(post);
+    });
+});
+
+
+//.. make sure / is in front of /posts/:post
+router.get('/posts/:post', function(req, res, next) {
+    req.post.populate('comments', function(err, post){
+        if (err){ return next(err) ;}
         res.json(post);
     });
 });
@@ -33,11 +45,18 @@ router.put('/posts/:post/upvote', function(req, res, next) {
     });
 });
 
+router.put('/posts/:post/comments/:comment/upvote', function(req, res, next) {
+    req.comment.upvote(function(err, comment){
+        if (err){return next(err);};
+        res.json(comment);
+    })
+})
+
 router.post('/posts/:post/comments', function(req, res, next) {
     var comment = new Comment(req.body);
 
     comment.post = req.post;
-    comment.save(function(err, post) {
+    comment.save(function(err, comment) {
         if (err){return next(err);}
 
         req.post.comments.push(comment);
@@ -53,9 +72,21 @@ router.param('post', function(req, res, next, id) {
 
     query.exec(function (err, post){
         if (err){return next(err);}
-        if (!post){return next(new Error('can\'t find post'));}
+        if (!post){return next(new Error("can't find post"));}
 
         req.post = post;
+        return next();
+    });
+});
+
+router.param('comment', function(req, res, next, id) {
+    var query = Comment.findById(id);
+
+    query.exec(function (err, comment){
+        if (err){return next(err);}
+        if (!comment){return next(new Error("can't find comment"));}
+
+        req.comment = comment;
         return next();
     });
 });
